801eded949dc577f0ca5229643f70716370d0056
slunchice <slunchice_@hotmail.com>
2018-03-28 17:23:55 -0400

    folders created

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Veggieburger');
+INSERT INTO burgers (burger_name) VALUES ('Double Veggieburger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Tofu Delight', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Fried Pepper Burger', true);
